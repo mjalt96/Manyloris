be0ed594aabaaad5be2ee08ebb70a0261c3c0d68
Mário Leitão-Teixeira <mjalt96@gmail.com>
2022-03-25 12:15:02 -0400

    Fix small issue

--- manyloris.py
+++ manyloris.py
@@ -27,7 +27,7 @@ def main() -> int:
 	# check that the required arguments number was fulfilled
 	if len(sys.argv) != 2:
 		print('\nYou need to provide a list of targets.\n\n====Example list====\n123.423.1.1:80:443\n12.1.612.123:80:443:8443\n231.23.522.3:443:80\n141.101.123.30:443\n\n-> Note that the list must have a target per line and the ports seperated by ":".\n\n====Usage====\n\npython3 manyloris [PATH TO LIST]\n\ne.g. python3 manyloris ~/Documents/targets.txt')
-		return 0
+		sys.exit()
 
 
 	with open(sys.argv[1]) as file:
@@ -39,7 +39,7 @@ def main() -> int:
 	start = input('\n\nWARNING! The Slowloris commands listed above are going to be executed.\nAre you sure that you want to proceed with %s targets? (Y/n)\n' % (len(targets)))
 
 	if start.lower() in ['n', 'no']: 
-		return 0
+		sys.exit()
 
 	time.sleep(2)
 
